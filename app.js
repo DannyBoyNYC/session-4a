b1747e8c27d2b6a721e0ee0f0191ba305234e63a
DannyBoyNYC <daniel.deverell@gmail.com>
2018-11-09 16:15:56 -0500

    done

--- app.js
+++ app.js
@@ -1,30 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-const recipeModels = require('./api/recipe.model');
-const recipes = require('./api/recipe.controllers');
-
 const app = express();
-const mongoUri = 'mongodb://devereld:dd2345@ds157223.mlab.com:57223/recipes-daniel';
 
-// this line always appears before any routes
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('app'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/app/index.html');
 });
 
-app.get('/api/recipes', recipes.findAll);
-app.get('/api/recipes/:id', recipes.findById);
-app.post('/api/recipes', recipes.add);
-app.put('/api/recipes/:id', recipes.update);
-app.delete('/api/recipes/:id', recipes.delete);
-app.get('/api/import', recipes.import);
-app.get('/api/killall', recipes.killall);
-
-mongoose.connect(mongoUri, { useNewUrlParser: true }, () => {
-  app.listen(3001);
-  console.log('Server running at http://localhost:3001/');
-});
+app.listen(3001);
+console.log('Server running at http://localhost:3001/');
